9abf5e5195e901840b1d9ce287405848c69fe522
Merott Movahedi <merott@merott.com>
2016-06-02 22:20:06 +0100

    feat(DI): register individual effect instances with the DI

--- lib/run-effects.ts
+++ lib/run-effects.ts
@@ -6,13 +6,23 @@ import { STATE_UPDATES_PROVIDER } from './state-updates';
 
 
 export function runEffects(...effects: any[]) {
-  const allEffects = flatten(effects)
+  const flattened = flatten(effects);
+
+  const individuals = flattened
+    .map(effectType => new Provider(effectType, {
+      useFactory: (effectInstances: any[]) =>
+        effectInstances.filter(instance => instance instanceof effectType)[0],
+      deps: [BOOTSTRAP_EFFECTS]
+    }));
+
+  const allEffects = flattened
     .map(effect => new Provider(BOOTSTRAP_EFFECTS, {
       useClass: effect,
       multi: true
     }));
 
   return [
+    ...individuals,
     ...allEffects,
     CONNECT_EFFECTS_PROVIDER,
     STATE_UPDATES_PROVIDER
